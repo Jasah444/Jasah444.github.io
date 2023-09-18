1325717366273cdacaeb74a00c8be9e0df69b594
Jasah444 <zS21004517@estudiantes.uv.mx>
2023-09-18 06:10:44 -0600

    commit a develop

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  xit(`should have as title 'myCV'`, () => {
+  xit(`should have as title 'mycv'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myCV');
-  });
+    expect(app.title).toEqual('mycv');
+  }); 
 
   xit('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myCV app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mycv app is running!');
   });
 });
